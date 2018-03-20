cef17a537feba0b6e28d725f7517ab31563e5bb0
Derrick DeConti <deconti.d@gmail.com>
2018-03-20 15:29:34 -0400

    Pushed BAM object init to bottom of stack.

--- amplicon_stats.py
+++ amplicon_stats.py
@@ -7,27 +7,35 @@ import matplotlib
 import re
 
 
-def plot_depth(pileups, locus):
+def plot_depth(pileups, locus, bams):
     """Plots depth across given loci."""
     plot_map = defaultdict(list)
-    #
+    filenames = [re.sub('\.bam$', '', bam) for bam in bams]
+    for pileup in pileups:
+        # TODO add a filepath
+        plot_file = '-'.join(locus) + '.png'
         
-def parse_bam_for_pileup(bams, loci):
+        
+
+        
+def parse_bam_for_pileup(bams, loci, buf):
     """Parses BAM for pileup data into map by locus key."""
     pileups = {}
     for locus in loci:
         chrom = locus[0]
         begin = int(locus[1])
         end = int(locus[2])
-        pileups[loci] = [get_pileup_vector(bam, chrom, begin, end)
+        pileups[loci] = [get_pileup_vector(bam, chrom, begin, end, buf)
                          for bam in bams]
     return pileups
 
 
-def get_pileup_vector(bam, chrom, begin, end):
+def get_pileup_vector(bam, chrom, begin, end, buf):
     """Get depth vector from bam given pos."""
     return [p.nsegments
-            for p in pysam.AlignmentFile(bam, 'rb').pileup(chrom, begin, end)]
+            for p in pysam.AlignmentFile(bam, 'rb').pileup(chrom,
+                                                           begin - buf,
+                                                           end + buf)]
 
 
 def main():
@@ -38,10 +46,13 @@ def main():
     parser.add_argument("-l", "--loci", required=True,
                         nargs=3, metavar=("chr", "begin", "end"),
                         action="append", help="comma-delimited loci [required]")
+    parser.add_argument("--buf", metavar="BUFFER", type=int, default=1000,
+                        help="buffer about the loci to include in plots")
     parser.add_argument("-b", "--bam", metavar="BAM", nargs='+',
                         required=True, help="BAM file(s) [required]")
     args = parser.parse_args()
     # central dispatch
+    parse_bam_for_pileup(args.bams, args.loci, args.buf)
     #plot_depth()
 
 
