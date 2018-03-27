d30920666806d31469362013f243c3731f23e8a2
Derrick DeConti <deconti.d@gmail.com>
2018-03-27 16:28:01 -0400

    Added locus cut sites depth plotting.

--- amplicon_stats.py
+++ amplicon_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import argparse
-from collections import defaultdict
 import numpy
 import matplotlib.pyplot as plt
 import pysam
@@ -9,16 +8,17 @@ import matplotlib
 import re
 
 
-def plot_depth(pileups, locus, bams, buf):
+def plot_depth_across_locus(pileups, locus, bams, buf):
     """Plots depth across given loci."""
     chrom, begin, end = locus
     begin = int(begin)
     filenames = [re.sub('\.bam$', '', bam) for bam in bams]
-    plot_file = '-'.join(locus) + '.png'
+    plot_file = '-'.join(locus) + '.across_whole_locus_depth.png'
+    # TODO add a file path
     fig, axarray = plt.subplots(nrows=len(pileups),
-                            ncols=1,
-                            figsize=(16, 4*len(pileups)),
-                            sharex=True)
+                                ncols=1,
+                                figsize=(16, 4*len(pileups)),
+                                sharex=True)
     if len(pileups) == 1:
         pileup = pileups[0]
         axarray.plot(range(begin - buf, len(pileup) + begin - buf), pileup)
@@ -27,7 +27,6 @@ def plot_depth(pileups, locus, bams, buf):
         axarray.set_title(filenames[0])
     else:
         for i, pileup in enumerate(pileups):
-            # TODO add a filepath
             axarray[i].plot(range(begin - buf, len(pileup) + begin - buf),
                             pileup)
             axarray[i].axvline(begin, color='r')
@@ -37,6 +36,40 @@ def plot_depth(pileups, locus, bams, buf):
     plt.close()
     return plot_file
 
+
+def plot_depth_about_locus_ends(pileups, locus, bams, buf):
+    """Plots depth about locus endpoints +/- buffer."""
+    chrom, begin, end = locus
+    begin = int(begin)
+    end = int(end)
+    filenames = [re.sub('\.bam$', '', bam) for bam in bams]
+    plot_file = '-'.join(locus) + '.cut_site_boundary_depth.png'
+    fig, axarray = plt.subplots(nrows=len(pileups),
+                                ncols=2,
+                                figsize=(8, 4*len(pileups)),
+                                sharey=True)
+    if len(pileups) == 1:
+        pileup = pileups[0]
+        print (begin + buf) - (begin - buf)
+        print len(pileup[:2*buf])
+        axarray[0].plot(range(begin - buf, begin + buf), pileup[:2*buf])
+        axarray[0].axvline(begin, color='r')
+        axarray[0].set_title(filenames[0] + " - 5\' of locus")
+        axarray[1].plot(range(end - buf, end + buf), pileup[-(2*buf):])
+        axarray[1].axvline(end, color='r')
+        axarray[1].set_title(filenames[0] + " - 3\' of locus")
+    else:
+        for i, pileup in enumerate(pileups):
+            axarray[i, 0].plot(range(begin - buf, begin + buf), pileup[:2*buf])
+            axarray[i, 0].plot(begin, color='r')
+            axarray[i, 0].set_title(filenames[0] + " - 5\' of locus")
+            axarray[i, 1].plot(range(end - buf, end + buf), pileup[-2*buf:])
+            axarray[i, 1].plot(end, color='r')
+            axarray[i, 1].set_title(filenames[0] + " - 3\' of locus")
+    fig.savefig(plot_file)
+    plt.close()
+    return plot_file
+ 
         
 def parse_bam_for_pileup(bams, loci, buf):
     """Parses BAM for pileup data into map by locus key."""
@@ -57,10 +90,6 @@ def get_pileup_vector(bam, chrom, begin, end, buf):
     for p in pysam.AlignmentFile(bam, 'rb').pileup(chrom, begin-buf, end+buf):
         depth[p.reference_pos - begin] = p.nsegments
     return depth
-    #return [p.nsegments
-    #        for p in pysam.AlignmentFile(bam, 'rb').pileup(chrom,
-    #                                                       begin - buf,
-    #                                                       end + buf)]
 
 
 def main():
@@ -81,7 +110,14 @@ def main():
     print args.loci
     pileups = parse_bam_for_pileup(args.bam, args.loci, args.buf)
     for locus in args.loci:
-        plot_depth(pileups[tuple(locus)], locus, args.bam, args.buf)
+        plot_depth_across_locus(pileups[tuple(locus)],
+                                locus,
+                                args.bam,
+                                args.buf)
+        plot_depth_about_locus_ends(pileups[tuple(locus)],
+                                    locus,
+                                    args.bam,
+                                    args.buf)
 
 
 if __name__ == "__main__":
